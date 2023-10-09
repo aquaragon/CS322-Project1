ddb8ffe31344d2f28b29f652d1472d3c989eda08
Aiden Duval <aduval@uoregon.edu>
2023-10-08 20:21:21 -0700

    I think we done

--- pageserver/pageserver.py
+++ pageserver/pageserver.py
@@ -20,9 +20,9 @@ logging.basicConfig(format='%(levelname)s:%(message)s',
 log = logging.getLogger(__name__)
 # Logging level may be overridden by configuration 
 
+import os
 import socket    # Basic TCP/IP communication on the internet
 import _thread   # Response computation runs concurrently with main program
-
 
 def listen(portnum):
     """
@@ -78,6 +78,10 @@ STATUS_NOT_FOUND = "HTTP/1.0 404 Not Found\n\n"
 STATUS_NOT_IMPLEMENTED = "HTTP/1.0 401 Not Implemented\n\n"
 
 
+
+        
+
+
 def respond(sock):
     """
     This server responds only to GET requests (not PUT, POST, or UPDATE).
@@ -91,8 +95,27 @@ def respond(sock):
 
     parts = request.split()
     if len(parts) > 1 and parts[0] == "GET":
-        transmit(STATUS_OK, sock)
-        transmit(CAT, sock)
+        link = parts[1]
+        link = link[1:]
+        print("LINK IS", link)
+        if '..' in link or '~' in link:
+            transmit(STATUS_FORBIDDEN, sock)
+        elif link == '':
+            transmit(STATUS_OK, sock)
+            transmit(CAT, sock)
+        else:
+            file_path = os.path.join("pages",link)
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                    response = STATUS_OK + content
+                    transmit(response, sock)
+            else:
+                transmit(STATUS_NOT_FOUND,sock)
+            
+            
+
+        
     else:
         log.info("Unhandled request: {}".format(request))
         transmit(STATUS_NOT_IMPLEMENTED, sock)
